9166e9d9beca676fff0cfa6a23088120334d2cbf
David Li <dtli@lcs.on.ca>
2023-05-25 16:30:54 -0400

    Add storing property and finish the presentation question section

--- MathMaven/Views/DivisionView.swift
+++ MathMaven/Views/DivisionView.swift
@@ -8,10 +8,58 @@
 import SwiftUI
 
 struct DivisionView: View {
+    //MARK: Storing property
+    
+    //Value to multiply
+    @State var secondValue = Int.random(in: 1...10)
+    @State var correctAnswer = Int.random(in: 1...10)
+    
+    //Hold input
+    @State var input = ""
+    
+    //user answer has checked?
+    @State var answerChecked = false
+    
+    //user answer is correct?
+    @State var answerCorrect = false
+    
+    //MARK: Computing property
+    
+    //Correct answer
+    var firstValue: Int{
+        return secondValue * correctAnswer
+    }
     var body: some View {
-        VStack {
-            Text("Division")
-            Text(Operation.division.rawValue)
+        
+        VStack(spacing: 0){
+            //1. Present question
+            PresentQuestionView(sign: Operation.division.rawValue, firstValue: firstValue, secondValue: secondValue)
+            
+            //2. Accept answer
+            HStack {
+
+                ZStack {
+                    
+                    // Only show this when the answer was found to be correct
+                    if answerCorrect == true {
+                        Image(systemName: "checkmark.circle")
+                            .foregroundColor(.green)
+                    }
+                    
+                    // Show this when the answer was checked and found to be false
+                    if answerChecked == true && answerCorrect == false {
+                        Image(systemName: "x.square")
+                            .foregroundColor(.red)
+                    }
+                }
+                
+                Spacer()
+                
+                TextField("",
+                          text: $input)
+                    .multilineTextAlignment(.trailing)
+            }
+            .padding(.horizontal)
         }
         .font(Font.custom("SF Pro", size: 64))
     }
