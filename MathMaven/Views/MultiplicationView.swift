571c389c1fd908f74f0a736c3bed8629d9eef354
David Li <dtli@lcs.on.ca>
2023-05-25 14:33:11 -0400

    Add storing property of multiplication View

--- MathMaven/Views/MultiplicationView.swift
+++ MathMaven/Views/MultiplicationView.swift
@@ -8,6 +8,28 @@
 import SwiftUI
 
 struct MultiplicationView: View {
+    //MARK: Storing property
+    
+    //Value to multiply
+    @State var firstValue = Int.random(in: 1...30)
+    @State var secondValue = Int.random(in: 1...30)
+    
+    //Hold input
+    @State var input = ""
+    
+    //user answer has checked?
+    @State var answerChecked = false
+    
+    //user answer is correct?
+    @State var answerCorrect = false
+    
+    //MARK: Computing property
+    
+    //Correct answer
+    var correctAnswer: Int{
+        return firstValue * secondValue
+    }
+    
     var body: some View {
         VStack {
             Text("Multiplication")
