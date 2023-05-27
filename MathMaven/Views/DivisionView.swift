eaf3e0c77f91695792f82ac42284800ecee33d17
David Li <dtli@lcs.on.ca>
2023-05-26 20:59:20 -0400

    Add firework animation when an answer is correct in Multiplication View

--- MathMaven/Views/DivisionView.swift
+++ MathMaven/Views/DivisionView.swift
@@ -30,66 +30,87 @@ struct DivisionView: View {
         return secondValue * correctResponse
     }
     var body: some View {
-        
-        VStack(spacing: 0){
-            //1. Present question
-            PresentQuestionView(sign: Operation.division.rawValue, firstValue: firstValue, secondValue: secondValue)
+        ZStack{
+            //animation
+            if answerCorrect == true {
+                 LottieView(animationNamed: "32585-fireworks-display")
+            }
             
-            //2. Accept answer
-            HStack {
+            VStack(spacing: 0){
+                //1. Present question
+                PresentQuestionView(sign: Operation.division.rawValue, firstValue: firstValue, secondValue: secondValue)
+                
+                //2. Accept answer
+                
+                //Animation
+                //ZStack{
+                   // if answerCorrect == true {
+                   //     LottieView(animationNamed: "32585-fireworks-display")
+                   // }
+                    HStack {
 
-                ZStack {
-                    
-                    // Only show this when the answer was found to be correct
-                    if answerCorrect == true {
-                        Image(systemName: "checkmark.circle")
-                            .foregroundColor(.green)
+                        ZStack {
+                            
+                            // Only show this when the answer was found to be correct
+                            if answerCorrect == true {
+                                Image(systemName: "checkmark.circle")
+                                    .foregroundColor(.green)
+                            }
+                            
+                            // Show this when the answer was checked and found to be false
+                            if answerChecked == true && answerCorrect == false {
+                                Image(systemName: "x.square")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                        
+                        Spacer()
+                        
+                        TextField("",
+                                  text: $input)
+                            .multilineTextAlignment(.trailing)
                     }
+                    .padding(.horizontal)
+                //}
+                //.frame(height: 100)
+                
+
+                
+                
+                //3. Check answer
+                //   Only show button when answer has not already been checked
+                if answerChecked == false {
                     
-                    // Show this when the answer was checked and found to be false
-                    if answerChecked == true && answerCorrect == false {
-                        Image(systemName: "x.square")
-                            .foregroundColor(.red)
-                    }
+                    CheckAnswerButtonView(input: input,
+                                          correctResponse: correctResponse,
+                                          answerChecked: $answerChecked,
+                                          answerCorrect: $answerCorrect)
+
+                } else {
+                    
+                    // 4. Generate new question
+                    // Only show this once an answer has been provided
+                    Button(action: {
+                        generateNewQuestion()
+                    }, label: {
+                        Text("New question")
+                            .font(.largeTitle)
+                    })
+                    .padding()
+                    .buttonStyle(.bordered)
+                    
                 }
                 
+                // Push interface up to top of screen
                 Spacer()
-                
-                TextField("",
-                          text: $input)
-                    .multilineTextAlignment(.trailing)
-            }
-            .padding(.horizontal)
-            
-            //3. Check answer
-            //   Only show button when answer has not already been checked
-            if answerChecked == false {
-                
-                CheckAnswerButtonView(input: input,
-                                      correctResponse: correctResponse,
-                                      answerChecked: $answerChecked,
-                                      answerCorrect: $answerCorrect)
 
-            } else {
-                
-                // 4. Generate new question
-                // Only show this once an answer has been provided
-                Button(action: {
-                    generateNewQuestion()
-                }, label: {
-                    Text("New question")
-                        .font(.largeTitle)
-                })
-                .padding()
-                .buttonStyle(.bordered)
-                
             }
+            .font(Font.custom("SF Pro", size: 64))
             
-            // Push interface up to top of screen
-            Spacer()
-
+            
         }
-        .font(Font.custom("SF Pro", size: 64))
+        
+        
     }
     
     //MARK: Function
